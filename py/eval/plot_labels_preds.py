fe1767ff28f3c8c6f9b3ec5bc3473832a9c38c2c
Aurelienne Jorge <ajorge@fuego.ssec.wisc.edu>
2024-08-28 02:03:12 +0000

    .

--- py/eval/plot_labels_preds.py
+++ py/eval/plot_labels_preds.py
@@ -1,16 +1,18 @@
 import pickle
 import sys
 from matplotlib import pyplot as plt
+import numpy as np
+import os
 
 pkl_file = sys.argv[1]
+figs_path = '/home/ajorge/lc_br/figs/samples/'
 
 p = pickle.load(open(pkl_file, 'rb'))
 for idx, preds in enumerate(p['preds']):
     fig, axs = plt.subplots(1,2)
-    #print(preds)
     #print(p['labels'][idx])
-    ax[0].imshow(p['labels'][idx])
-    ax[1].imshow(preds)
-    plt.show()
-    break
-
+    axs[0].imshow(p['labels'][idx])
+    axs[0].set_title('Target')
+    axs[1].imshow(preds.astype(np.float32), vmin=0, vmax=1)
+    axs[1].set_title('Prediction')
+    plt.savefig(os.path.join(figs_path, f"{idx}.png")) 
